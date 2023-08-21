325a6ecae066a9abd9100d79d0ef73e9d9c17847
BenKenobie31415 <villauben@gmail.com>
2023-08-21 21:54:34 +0200

    Added nvim config

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,12 @@
+local o = vim.opt
+
+o.clipboard = "unnamedplus"
+o.undofile = true
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.termguicolors = true
+o.wrap = false
+o.title = true
+o.number = true
+o.relativenumber = true
